7d94298bae25ca864e35b6285692cfb9dce66a5b
naude <naude_serch@msn.om>
2021-09-07 11:21:16 -0500

    arreglando puerto

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ const bodyParser = require('body-parser');
 
 const app = express();
 
-const port = process.env.PORT;
+const port = process.env.PORT || 8080;
 
 app.use(bodyParser.urlencoded({ extended: true }));
 
